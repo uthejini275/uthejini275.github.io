e0f999b6b7f0635d056b1494c84936884bba462e
uthejini275 <uthejini275petti@gmail.com>
2025-06-05 19:34:52 +0530

    updated

--- p2/script.js
+++ p2/script.js
@@ -1,23 +1,35 @@
 const showLogin = () => {
-    let str=`
+  let str = `
     <div class='App-Container'>
-    <h1>Login Form</h1>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <p><button>Log In</button></p>
-    <p><button onclick='showRegister()'>Create Account</button></p>
+      <h1>Login Form</h1>
+      <p><input type="text" id="txtEmail" placeholder="Email"></p>
+      <p><input type="password" id="txtPass" placeholder="Password"></p>
+      <p><button onclick="login()">Log In</button></p>
+      <p><button onclick="showRegister()">Create Account</button></p>
     </div>
-    `
-    root.innerHTML = str
-}
+    <div id="message"></div>
+  `;
+  root.innerHTML = str;
+};
 
 const showRegister = () => {
-      let str=`
-    <h1>Register Form</h1>
-    <p><input type="text" id="txtName"></p>
-     <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <button>Register</button>
-    `
-    root.innerHTML = str
-}
+  let str = `
+    <div class='App-Container'>
+      <h1>Register Form</h1>
+      <p><input type="text" id="txtName" placeholder="Name"></p>
+      <p><input type="text" id="txtEmail" placeholder="Email"></p>
+      <p><input type="password" id="txtPass" placeholder="Password"></p>
+      <p><button onclick="register()">Register</button></p>
+    </div>
+    <div id="message"></div>
+  `;
+  root.innerHTML = str;
+};
+
+const login = () => {
+  document.getElementById("message").innerText = "Welcome!";
+};
+
+const register = () => {
+  document.getElementById("message").innerText = "Account Created!";
+};
